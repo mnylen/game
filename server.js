592b1ea9a85f9695d56497bbeb737703d2dff329
Mikko Nylén <mikko.nylen@gmail.com>
2014-09-17 21:37:11 +0300

    Initial commit with DualShock 3 left & right analog stick events broadcasted to browser through socket.io

---
+++ server.js
@@ -0,0 +1,25 @@
+var app        = require('express')();
+var http       = require('http').Server(app);
+var io         = require('socket.io')(http);
+var dualShock  = require('dualshock-controller');
+var path       = require('path');
+var controller = dualShock();
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+});
+
+var events = ['error', 'left:move', 'right:move'];
+events.forEach(function(eventName) {
+    controller.on(eventName, function(data) {
+        io.emit(eventName, data);
+    });
+});
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root : path.join(__dirname, 'public') });
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
